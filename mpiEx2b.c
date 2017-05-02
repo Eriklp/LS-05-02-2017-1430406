0cdaad7618879a7fe0911507d7b935fbdda51424
invitado <invitado@s1pc9.eisc.univalle.edu.co>
2017-05-02 15:48:15 -0500

    desarrollo del punto 2 mejora

--- mpiEx2b.c
+++ mpiEx2b.c
@@ -10,8 +10,8 @@ int main(int argc, char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);	
 	if(rank == 0){
-		printf("Yo soy el maestro %d! \n", rank);
-		printf("procesos corriendo: %d", size);
+		printf("Yo soy el maestro %d: Y el numero de procesos es: %d ",rank ,size);
+		//printf("procesos corriendo: %d", size);
 	}
 	else{
 		printf("soy un proceso con identificador %d! \n", rank);

158ce8bf66d58c6e52bca7412b40d65d469b954a
invitado <invitado@s1pc9.eisc.univalle.edu.co>
2017-05-02 15:32:00 -0500

    primer ejemplo desarrollado

---
+++ mpiEx2a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+	int rank, size;
+
+	MPI_Init(&argc, &argv);
+	
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if(rank == 0){
+		printf("Yo soy el maestro %d! \n", rank);
+	}
+	else{
+		printf("soy un proceso con identificador %d! \n", rank);
+	}
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);	
+
+	MPI_Finalize();
+	return 0;
+}

cd4a42b8a7736dcb19cabe196e9cc683fe9f7c1d
Eriklp <Eriklp@github.com>
2017-05-03 11:04:32 -0500

    ejercicio 4

---
+++ mpiEx4a.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "mpi.h"
+
+int main(int argc,char *argv[]){
+	int i, sum, sumTotal, upToVal;
+	int start, end, size, rank;
+
+	upToVal = 10000;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	// Defina aqui el segmento que debe procesar una tarea
+	// El inicio del segmento en la variable 'start', el fin del segmento
+	// la variable 'end'
+    	sum = 0;
+    	for(i=start; i<= end; i++){
+    		sum = sum +i;
+    	}
+	// Utilice la funcion 'MPI_Reduce' para guardar en la variable 
+	// 'sumTotal' la suma parcial de todos las tareas 
+	printf ("\nTotal: %d\n",sumTotal);
+
+	MPI_Finalize();
+	
+	return 0;
+}
+
